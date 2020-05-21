7a64d6ce4afae03b1867f2776cf4cf095726ba48
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-05-21 11:36:44 +0200

    new newsticker feeds

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2191,7 +2191,10 @@
   (newsticker-url-list
    '(("Mastering Emacs" "http://www.masteringemacs.org/feed")
      ("Rust Lang" "https://blog.rust-lang.org/feed.xml")
+     ("WargarW" "https://queryfeed.net/tw?q=%40WargarW")
      ("Gandalf the Ghost" "https://queryfeed.net/tw?q=%40GandalftheWhi19")
+     ("Marie Cachet" "https://www.youtube.com/feeds/videos.xml?channel_id=UCLM6JlQkQ9tltBL9Bipci9g")
+     ("Justus Walker" "https://www.youtube.com/feeds/videos.xml?user=senttosiberia")
      ("Luke Smith Blog" "https://lukesmith.xyz/rss.xml")
      ("Tsoding" "https://www.youtube.com/feeds/videos.xml?channel_id=UCEbYhDd6c6vngsF5PQpFVWg")
      ("Protesilaos Stavrou" "https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g")
