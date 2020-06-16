ab4f935037c07cc656996e093e85935c25e9347e
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-06-16 17:23:00 +0200

    removed some .files from home

--- lein/.cache/lein/profiles.clj
+++ lein/.cache/lein/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [
+{:user {:plugins [[luminus/lein-template "2.9.9.2"]
                   ;; [jonase/eastwood "0.2.1"]
                   ;; [cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   ;; [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [luminus/lein-template "2.9.9.2"]
                   ]}}
