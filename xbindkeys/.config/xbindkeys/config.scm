bf8a4b8d294697af84e1a8cd4313a8c3b1b5c3c0
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-10-15 23:42:04 +0200

    xbindkeys: refactored config

--- xbindkeys/.config/xbindkeys/config.scm
+++ xbindkeys/.config/xbindkeys/config.scm
@@ -18,8 +18,7 @@
 ;; (use-modules (system repl server))
 ;; (spawn-server)
 
-(use-modules (srfi srfi-1)
-             (ice-9 format))
+(use-modules (srfi srfi-1) (ice-9 format))
 
 (define chain "")
 (define global-map (make-hash-table))
@@ -66,15 +65,11 @@ command may be a string or a function"
     (set! current (if (hash-table? val) val global-map))))
 
 (define (hash-map-keys hm)
-  (define (hash-map-keys-inner hm)
-    (apply
-     append!
-     (hash-map->list
-      (lambda (key value)
-        (cons key (if (hash-table? value) (hash-map-keys-inner value) '())))
-      hm)))
-
-  (delete-duplicates! (hash-map-keys-inner hm)))
+  (define (inner hm)
+    (apply append! (hash-map->list
+                    (lambda (key value) (cons key (if (hash-table? value) (inner value) '())))
+                    hm)))
+  (delete-duplicates! (inner hm)))
 
 (define (bind-function key ksym)
   (xbindkey-function
@@ -89,26 +84,71 @@ command may be a string or a function"
            (set! chain (string-trim (string-append chain " " key)))
            (run-command (string-append "sratus -u keyseq -v '" chain "'")))))))
 
+(define (permutate lst)
+  (define (inner pfx lst)
+    (if (null? lst)
+        (list pfx)
+        (let ((op (car lst)) (tail (cdr lst)))
+          (case op
+            ((and) (handle-and pfx tail))
+            ((or) (handle-or pfx tail))
+            (else (error "List should start with and/or."))))))
+
+  (define (handle-and pfx lst)
+    (if (null? lst)
+        (list pfx)
+        (let ((head (car lst)) (tail (cdr lst)))
+          (cond ((string? head)
+                 (handle-and (string-append pfx head) tail))
+                ((number? head)
+                 (handle-and (string-append pfx (number->string head)) tail))
+                ((symbol? head)
+                 (handle-and (string-append pfx (symbol->string head)) tail))
+                ((list? head)
+                 (apply append! (map (lambda (p) (handle-and p tail)) (inner pfx head))))
+                (else (error "Neither string or list or number in and"))))))
+
+  (define (handle-or pfx lst)
+    (if (null? lst)
+        (list pfx)
+        (apply append!
+               (map (lambda (e) (cond ((string? e) (list (string-append pfx e)))
+                                 ((number? e) (list (string-append pfx (number->string e))))
+                                 ((symbol? e) (list (string-append pfx (symbol->string e))))
+                                 ((list? e) (inner pfx e))
+                                 (else (error "Neither string or list or number in or"))))
+                    lst))))
+
+  (if (null? (cdr lst))
+      (list)
+      (inner "" lst)))
+
 (define (main)
   (for-each (lambda (key)
               (let ((ksym (kbd key)))
                 (bind-function key ksym)
                 (bind-function key (cons "Mod2" ksym))))
             (delete! "s-g" (hash-map-keys global-map)))
+  (xbindkey-function (kbd "s-g")
+                     (λ ()
+                       (set! current global-map)
+                       (when (not (string= chain ""))
+                         (set! chain "")
+                         (run-command "sratus -u keyseq -v ''")))))
 
-  (xbindkey-function
-   (kbd "s-g")
-   (λ ()
-     (set! current global-map)
-     (when (not (string= chain ""))
-       (set! chain "")
-       (run-command "sratus -u keyseq -v ''")))))
-
-(define (define-key key cmd) (global-set-key key cmd))
 (define prefix "")
+(define-macro (define-key key cmd)
+  ;; (global-set-key key cmd)
+  (cond
+   ((and (string? key) (string? cmd))
+    `(global-set-key (string-trim (string-append prefix " " ,key)) ,cmd))
+   ((and (list? key) (list? cmd))
+    `(begin . ,(map (lambda (k c) `(global-set-key (string-trim (string-append prefix " " ,k)) ,c))
+                    (permutate key) (permutate cmd))))
+   (else (error "Neither string or list in define-key"))))
+
 (define-macro (with-prefix prefix exp . body)
-  `(let* ((prefix (string-trim (string-append prefix " " ,prefix)))
-          (define-key (lambda (key cmd) (global-set-key (string-append prefix " " key) cmd))))
+  `(let ((prefix (string-trim (string-append prefix " " ,prefix))))
      ,exp . ,body))
 
 ;;; bspwm indipendent keys ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -121,21 +161,12 @@ command may be a string or a function"
 (define-key "XF86MonBrightnessUp" "xbacklight -inc 3")
 
 ;; change volume
-(define-key "XF86AudioLowerVolume" "amixer -D pulse sset Master 2%- >/dev/null")
-(define-key "XF86AudioMute" "amixer -D pulse sset Master toggle >/dev/null")
-(define-key "XF86AudioRaiseVolume" "amixer -D pulse sset Master 2%+ >/dev/null")
+(define-key (or XF86AudioLowerVolume XF86AudioMute XF86AudioRaiseVolume)
+  (and "amixer -D pulse sset Master " (or 2%- toggle 2%+) " >/dev/null"))
 
 ;; change volume
-(define-key "s-1" "amixer -D pulse sset Master  10% >/dev/null")
-(define-key "s-2" "amixer -D pulse sset Master  20% >/dev/null")
-(define-key "s-3" "amixer -D pulse sset Master  30% >/dev/null")
-(define-key "s-4" "amixer -D pulse sset Master  40% >/dev/null")
-(define-key "s-5" "amixer -D pulse sset Master  50% >/dev/null")
-(define-key "s-6" "amixer -D pulse sset Master  60% >/dev/null")
-(define-key "s-7" "amixer -D pulse sset Master  70% >/dev/null")
-(define-key "s-8" "amixer -D pulse sset Master  80% >/dev/null")
-(define-key "s-9" "amixer -D pulse sset Master  90% >/dev/null")
-(define-key "s-0" "amixer -D pulse sset Master 100% >/dev/null")
+(define-key (and s- (or 1 2 3 4 5 6 7 8 9 0))
+  (and "amixer -D pulse sset Master " (or 1 2 3 4 5 6 7 8 9 10) "0% >/dev/null"))
 
 ;; open programs
 (with-prefix "s-o"
@@ -150,29 +181,15 @@ command may be a string or a function"
 
 ;; emacs progs
 (with-prefix "s-e"
-  (define-key "s-e" "em")
-  (define-key "s-r" "emremember")
-  (define-key "s-S-r" "emremember-notes"))
+  (define-key (or s-e s-r s-S-r) (or em emremember emremember-notes)))
 
 ;; mpd control
 (with-prefix "s-s"
-  (define-key "s-n" "mpc -q next")
-  (define-key "s-p" "mpc -q prev")
-  (define-key "s-t" "mpc -q toggle")
+  (define-key (or s-n s-p s-t) (and "mpc -q " (or next prev toggle)))
 
   ;; mpd volume
-  (define-key "s-comma" "mpc -q volume -1")
-  (define-key "s-period" "mpc -q volume +1")
-  (define-key "s-1" "mpc -q volume 1")
-  (define-key "s-2" "mpc -q volume 2")
-  (define-key "s-3" "mpc -q volume 3")
-  (define-key "s-4" "mpc -q volume 4")
-  (define-key "s-5" "mpc -q volume 5")
-  (define-key "s-6" "mpc -q volume 6")
-  (define-key "s-7" "mpc -q volume 7")
-  (define-key "s-8" "mpc -q volume 8")
-  (define-key "s-9" "mpc -q volume 9")
-  (define-key "s-0" "mpc -q volume 10"))
+  (define-key (and s- (or comma period 1 2 3 4 5 6 7 8 9 0))
+    (and "mpc -q volume " (or -1 "+1" (and (or 1 2 3 4 5 6 7 8 9 10) 0)))))
 
 ;; keyboard layouts control
 (define-key "s-k" "switch_keyboard next")
@@ -180,12 +197,8 @@ command may be a string or a function"
 
 ;; change panel mode
 (with-prefix "s-p"
-  (define-key "s-d" "runel -m default")
-  (define-key "s-s" "runel -m mpd")
-  (define-key "s-h" "runel -m hardware")
-  (define-key "s-n" "runel -m network")
-  (define-key "s-w" "runel -m wifi")
-  (define-key "s-c" "runel -m corona"))
+  (define-key (and s- (or d s h n w c))
+    (and "runel -m " (or default mpd hardware network wifi corona))))
 
 ;;; nodes (windows) commands ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (with-prefix "s-w"
@@ -193,64 +206,37 @@ command may be a string or a function"
   (define-key "s-c" "bspc node --close")
 
   ;; states
-  (define-key "s-a" "bspc node --state tiled")
-  (define-key "s-o" "bspc node --state pseudo_tiled")
-  (define-key "s-e" "bspc node --state floating")
-  (define-key "s-u" "bspc node --state fullscreen")
+  (define-key (and s- (or a o e u))
+    (and "bspc node --state " (or tiled pseudo_tiled floating fullscreen)))
 
   ;; send to desktop by name
   (with-prefix "s-t"
-    (define-key "s-b" "bspc node --to-desktop B")
-    (define-key "s-c" "bspc node --to-desktop C")
-    (define-key "s-h" "bspc node --to-desktop H")
-    (define-key "s-m" "bspc node --to-desktop M")
-    (define-key "s-r" "bspc node --to-desktop R")
-    (define-key "s-s" "bspc node --to-desktop S")
-    (define-key "s-t" "bspc node --to-desktop T")
-    (define-key "s-v" "bspc node --to-desktop V")
+    (define-key (and s- (or b c h m r s t v))
+      (and "bspc node --to-desktop " (or B C H M R S T V)))
 
-    (define-key "s-S-b" "bspc node --to-desktop B --follow")
-    (define-key "s-S-c" "bspc node --to-desktop C --follow")
-    (define-key "s-S-h" "bspc node --to-desktop H --follow")
-    (define-key "s-S-m" "bspc node --to-desktop M --follow")
-    (define-key "s-S-r" "bspc node --to-desktop R --follow")
-    (define-key "s-S-s" "bspc node --to-desktop S --follow")
-    (define-key "s-S-t" "bspc node --to-desktop T --follow")
-    (define-key "s-S-v" "bspc node --to-desktop V --follow")))
-
+    (define-key (and s-S- (or b c h m r s t v))
+      (and "bspc node --to-desktop " (or B C H M R S T V) " --follow"))))
 
 ;; focus by direction
-(define-key "s-c" "bspc node --focus north")
-(define-key "s-h" "bspc node --focus west")
-(define-key "s-n" "bspc node --focus east")
-(define-key "s-t" "bspc node --focus south")
-(define-key "s-Tab" "bspc node --focus prev.local.!hidden.window")
-(define-key "s-S-Tab" "bspc node --focus next.local.!hidden.window")
+(define-key (and s- (or c h n t))
+  (and "bspc node --focus " (or north west east south)))
+(define-key (or s-Tab s-S-Tab)
+  (and "bspc node --focus " (and (or prev next) .local.!hidden.window)))
 
 ;; euclid mover
-(define-key "s-S-c" "bspwm_move north 10")
-(define-key "s-S-h" "bspwm_move west 10")
-(define-key "s-S-n" "bspwm_move east 10")
-(define-key "s-S-t" "bspwm_move south 10")
+(define-key (and s-S- (or c h n t))
+  (and "bspwm_move " (or north west east south) " 10"))
 
-(define-key "s-bracketleft" "bspwm_resize north 5")
-(define-key "s-bracketright" "bspwm_resize south 5")
-(define-key "s-S-bracketleft" "bspwm_resize west 5")
-(define-key "s-S-bracketright" "bspwm_resize east 5")
+(define-key (and (or s- s-S-) bracket (or left right))
+  (and "bspwm_resize " (or north south west east) " 5"))
 
 ;;; desktop commands ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (with-prefix "s-d"
   (define-key "s-Tab" "bspc desktop --layout next")
 
   ;; focus desktop
-  (define-key "s-b" "bspc desktop --focus B")
-  (define-key "s-c" "bspc desktop --focus C")
-  (define-key "s-h" "bspc desktop --focus H")
-  (define-key "s-m" "bspc desktop --focus M")
-  (define-key "s-r" "bspc desktop --focus R")
-  (define-key "s-s" "bspc desktop --focus S")
-  (define-key "s-t" "bspc desktop --focus T")
-  (define-key "s-v" "bspc desktop --focus V"))
+  (define-key (and s- (or b c h m r s t v))
+    (and "bspc desktop --focus " (or B C H M R S T V))))
 
 ;; This one creates all bindings from global-map.
 ;; Should run at the end
