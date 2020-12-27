8d693936a64f5dfd78e9a0ef91f70438f7676141
xFA25E <55886566+xFA25E@users.noreply.github.com>
2020-12-27 21:58:33 +0100

    stumpwm: colors experiments

--- stumpwm/.config/stumpwm/init.lisp
+++ stumpwm/.config/stumpwm/init.lisp
@@ -1,11 +1,36 @@
 (in-package :stumpwm)
 
+(setf *colors*
+      ;; '("black" "red"    "green"   "magenta" "blue"    "yellow"  "cyan"    "white"))
+      '("#101010" "#ff0086" "#00c918" "#ad00a1" "#3777e6" "#aba800" "#1faaaa" "#ffffff"))
+
+(dolist (s *screen-list*)
+  (update-color-map s))
+
+;; (let ((white (nth 7 *colors*))
+;;       (black (nth 0 *colors*))
+;;       (yellow (nth 5 *colors*))
+;;       (blue (nth 4 *colors*))
+;;       (green (nth 2 *colors*)))
+;;   (set-bg-color black)
+;;   (set-border-color white)
+;;   (set-fg-color white)
+;;   (set-float-focus-color yellow)
+;;   (set-float-unfocus-color blue)
+;;   (set-focus-color green)
+;;   (set-unfocus-color white)
+;;   (set-win-bg-color black))
+;; (update-colors-all-screens)
+
 (set-font "-*-terminus-medium-r-*-*-24-*-*-*-*-*-iso10646-1")
 
 (clear-window-placement-rules)
 
-(setf *screen-mode-line-format* "^B^70[%n] %t L%l V%a B%b %d")
+(setf *screen-mode-line-format* "[%n] %t^> L%l V%a B%b %d")
 
+(setf *mode-line-border-color* (nth 0 *colors*))
+(setf *mode-line-foreground-color* (nth 7 *colors*))
+(setf *mode-line-background-color* (nth 0 *colors*))
 (setf *mode-line-border-width* 0)
 (setf *mode-line-pad-x* 0)
 (setf *mode-line-pad-y* 0)
