6078107c14a890bcdaee42c9a9a49482960d20d8
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-03-10 13:06:02 +0100

    browser: added ytdl and mpv

--- overlays/browser.nix
+++ overlays/browser.nix
@@ -2,10 +2,12 @@ self: super: {
   browser = super.writeShellScriptBin "browser" ''
     browser=""
     while test -z "''${browser}"; do
-        case "$(printf 'qutebrowser\nfirefox\nchromium\n' | '${self.dmenu}/bin/dmenu')" in
+        case "$(printf 'qutebrowser\nfirefox\nchromium\nytdl\nmpv\n' | '${self.dmenu}/bin/dmenu')" in
             qutebrowser) browser=qutebrowser ;;
             firefox) browser="${self.firefox}/bin/firefox" ;;
             chromium) browser="${self.ungoogledChromiumIncognito}/bin/chromium-incognito" ;;
+            ytdl) browser="${self.scripts}/bin/ytdli" ;;
+            mpv) browser="${self.scripts}/bin/mpvi" ;;
         esac
     done
     "''${browser}" "$@"
