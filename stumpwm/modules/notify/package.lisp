3517af31790feb2f9a5c68dc4504cc44c743931b
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-03-10 11:44:37 +0100

    deleted notify module

--- stumpwm/modules/notify/package.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage #:notify
-  (:use #:cl
-	#:dbus
-	#:bordeaux-threads)
-  (:export #:*notification-received-hook*
-	   #:notify-server-toggle
-           #:notify-server-on
-           #:notify-server-off))
