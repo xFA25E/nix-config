bde0aa0922131f81762ee68fec0784ca588922e4
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-03-22 00:23:46 +0100

    nixpkgs: pinned rimer build expression

--- overlays/rimer.nix
+++ overlays/rimer.nix
@@ -1,6 +1,6 @@
 self: super: {
   rimer = import (super.fetchurl {
-    url = "https://raw.githubusercontent.com/xFA25E/rimer/master/default.nix";
+    url = "https://raw.githubusercontent.com/xFA25E/rimer/eb984f1b5a313e9c1ca46e995403d747eb50c2bb/default.nix";
     sha256 = "1rxihdnbpmkg4zvs2s1l20sv1n4r9xqs50yfayqd26s1s060n5z0";
   }) { pkgs = self; };
 }
