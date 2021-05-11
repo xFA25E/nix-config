b7e61717e546b9ff58fd375c44c2111295d77703
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-05-11 16:45:28 +0200

    nixpkgs: back to rofi, no more dmenu

--- overlays/dmenu/default.nix
+++ overlays/dmenu/default.nix
@@ -1,3 +1,11 @@
 self: super: {
-  dmenu = super.dmenu.override { patches = [ ./dmenu.patch ]; };
+  # dmenu = super.dmenu.override { patches = [ ./dmenu.patch ]; };
+  dmenu = super.symlinkJoin {
+    name = "rofi-with-dmenu";
+    paths = [ super.rofi ];
+    nativeBuildInputs = [ super.makeWrapper ];
+    postBuild = ''
+      makeWrapper $out/bin/rofi $out/bin/dmenu --add-flags -dmenu
+    '';
+  };
 }
