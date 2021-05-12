0395d5e1a61301a554e575b4e3c1c3f203d7ec1e
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-05-12 14:35:48 +0200

    nixpkgs: back to dmenu
    nixos repos already had a patch that fixes focus stealing.  I was overwriting it
    with my patches

--- overlays/dmenu/default.nix
+++ overlays/dmenu/default.nix
@@ -1,11 +1,5 @@
 self: super: {
-  # dmenu = super.dmenu.override { patches = [ ./dmenu.patch ]; };
-  dmenu = super.symlinkJoin {
-    name = "rofi-with-dmenu";
-    paths = [ super.rofi ];
-    nativeBuildInputs = [ super.makeWrapper ];
-    postBuild = ''
-      makeWrapper $out/bin/rofi $out/bin/dmenu --add-flags -dmenu
-    '';
-  };
+  dmenu = super.dmenu.overrideAttrs (attrs: {
+    patches = attrs.patches ++ [ ./dmenu.patch ];
+  });
 }
