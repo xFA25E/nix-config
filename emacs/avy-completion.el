915edeb446483f88fd6de96b55477a0536a4da98
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-06-08 16:05:12 +0200

    Emacs: avy-completion refactoring

--- emacs/avy-completion.el
+++ emacs/avy-completion.el
@@ -13,15 +13,18 @@
   (interactive)
   (if-let* ((wnd (get-buffer-window "*Completions*" 0))
             (wnd-start (window-start wnd))
-            (wnd-end (window-end wnd t)))
-      (with-current-buffer "*Completions*"
-        (avy-with avy-completion
-          (let ((avy-action #'avy-action-choose))
-            (avy-process
-             (save-excursion
-               (cl-loop initially (progn (goto-char wnd-start) (previous-completion 1))
-                        do (next-completion 1) while (< (point) wnd-end)
-                        collect (cons (point) wnd)))))))
+            (wnd-end (window-end wnd t))
+            (candidates
+             (with-current-buffer "*Completions*"
+               (save-excursion
+                 (goto-char wnd-start)
+                 (previous-completion 1)
+                 (cl-loop do (next-completion 1)
+                          while (< (point) wnd-end)
+                          collect (cons (point) wnd))))))
+      (avy-with avy-completion
+        (let ((avy-action #'avy-action-choose))
+          (avy-process candidates)))
     (user-error "No *Completions* windows")))
 
 (provide 'avy-completion)
