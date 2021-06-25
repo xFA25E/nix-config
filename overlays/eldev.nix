bde40fb9f562c93a6abdf5a7c5be29164dccaeed
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-06-25 17:26:26 +0200

    nixpkgs: updated eldev version

--- overlays/eldev.nix
+++ overlays/eldev.nix
@@ -1,16 +1,16 @@
 self: super: {
   eldev = super.stdenv.mkDerivation rec {
     pname = "eldev";
-    version = "0.8.1";
+    version = "0.9.1";
     src = super.fetchurl {
       url = "https://raw.githubusercontent.com/doublep/eldev/${version}/bin/eldev";
-      sha256 = "0csn6c4w95iswqdlj5akzspcm5ar7imngqcdch87ac21wz8xigln";
+      sha256 = "0ikhhfxm1rz3wp37spsy8bcnx5071ard71pd1riw09rsybilxhgn";
     };
     nativeBuildInputs = [ super.makeWrapper ];
     unpackPhase = "true";
     installPhase = ''
       install -D -v -m555 "$src" "$out/bin/eldev"
-      wrapProgram "$out/bin/eldev" --set ELDEV_EMACS "${super.emacs}/bin/emacs"
+      wrapProgram "$out/bin/eldev" --set ELDEV_EMACS "${self.emacs}/bin/emacs"
     '';
   };
 }
