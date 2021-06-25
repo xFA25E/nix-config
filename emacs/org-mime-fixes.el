ec0d13f7f269ea469e7ed54d4dd4c710947a39e2
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-06-25 17:26:41 +0200

    Emacs: org-mime-fixes: no more "edit upto signature"

--- emacs/org-mime-fixes.el
+++ emacs/org-mime-fixes.el
@@ -24,41 +24,7 @@ For some reason it is added twice to the src attribute."
   (cl-destructuring-bind (first . rest) args
     (cons (replace-regexp-in-string "src=\"file:///" "src=\"/" first) rest)))
 
-(defun org-mime-edit-mail-in-org-mode-up-to-signature ()
-  "Dont add signature to org mime edit buffer."
-  (interactive)
-  ;; see `org-src--edit-element'
-  (cond
-   ((eq major-mode 'org-mode)
-    (message "This command is not for `org-mode'."))
-   (t
-    (setq org-mime--saved-temp-window-config (current-window-configuration))
-    (let* ((beg (copy-marker (org-mime-mail-body-begin)))
-           (end (copy-marker (or (org-mime-mail-signature-begin)
-                                 (point-max))))
-           (bufname "OrgMimeMailBody")
-           (buffer (generate-new-buffer bufname))
-           (overlay (org-mime-src--make-source-overlay beg end))
-           (text (buffer-substring-no-properties beg end)))
-
-      (setq org-mime-src--beg-marker beg)
-      (setq org-mime-src--end-marker end)
-      ;; don't use local-variable because only user can't edit multiple emails
-      ;; or multiple embedded org code in one mail
-      (setq org-mime-src--overlay overlay)
-
-      (save-excursion
-        (delete-other-windows)
-        (org-switch-to-buffer-other-window buffer)
-        (erase-buffer)
-        (insert org-mime-src--hint)
-        (insert text)
-        (goto-char (point-min))
-        (org-mode)
-        (org-mime-src-mode))))))
-
 (advice-add 'org-mime-beautify-quoted :filter-return #'org-mime-beautify-quoted-add-newlines)
 (advice-add 'org-mime-replace-images :filter-args #'org-mime-replace-images-fix-cids-and-path)
-(advice-add 'org-mime-edit-mail-in-org-mode :override #'org-mime-edit-mail-in-org-mode-up-to-signature)
 
 (provide 'org-mime-fixes)
