8ab0dee842e64bfe9a0bd019e2d00378032f8133
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-08-30 17:41:02 +0200

    nixpkgs: added format duration to ytdly

--- overlays/ytdl.nix
+++ overlays/ytdl.nix
@@ -1,11 +1,26 @@
 self: super: let
+  formatDurationFilename = super.writeShellScript "format_duration_filename" ''
+    if [[ ''${1:?provide file} =~ ([0-9]+)(\.[^.]+)$ ]]; then
+        full="''${BASH_REMATCH[0]}"
+        dur="''${BASH_REMATCH[1]}"
+        ext="''${BASH_REMATCH[2]}"
+
+        scs="$((dur % 60))"
+        mns="$(((dur / 60) % 60))"
+        hrs="$((dur / (60 * 60)))"
+
+        printf -v newfile "%s%02d:%02d:%02d%s" "''${1:0:-''${#full}}" "$hrs" "$mns" "$scs" "$ext"
+        ${self.coreutils}/bin/mv -v "$1" "$newfile"
+    fi
+  '';
+
   ytdl-dir = "\${YTDL_DIR:-\${XDG_VIDEOS_DIR:-\${HOME}/Videos}/youtube}";
   title-ext-fmt = "%(title)s.%(ext)s";
   video-title-fmt = "%(upload_date)s - ${title-ext-fmt}";
   playlist-index-fmt = "%(playlist_uploader)s - %(playlist)s - %(playlist_index)s";
   playlist-video-fmt = "${playlist-index-fmt}${video-title-fmt}";
   playlist-audio-fmt = "${playlist-index-fmt} - ${title-ext-fmt}";
-  youtube-video-fmt = "${ytdl-dir}/%(uploader)s - ${video-title-fmt}";
+  youtube-video-fmt = "${ytdl-dir}/%(uploader)s - %(upload_date)s - %(title)s - %(duration)s.%(ext)s";
 in {
   ytdla = super.writeShellScriptBin "ytdla" ''
     exec "${self.youtube-dl}/bin/youtube-dl" --format "bestaudio/best" --extract-audio "$@"
@@ -17,10 +32,10 @@ in {
     exec "${self.ytdla}/bin/ytdla" --output "${playlist-video-fmt}" --yes-playlist "$@"
   '';
   ytdly = super.writeShellScriptBin "ytdly" ''
-    exec "${self.youtube-dl}/bin/youtube-dl" --output "${youtube-video-fmt}" "$@"
+    exec "${self.youtube-dl}/bin/youtube-dl" --output "${youtube-video-fmt}" --exec "${formatDurationFilename} {}" "$@"
   '';
   ytdlay = super.writeShellScriptBin "ytdlay" ''
-    exec "${self.ytdla}/bin/ytdla" --output "${youtube-video-fmt}" "$@"
+    exec "${self.ytdla}/bin/ytdla" --output "${youtube-video-fmt}" --exec "${formatDurationFilename} {}" "$@"
   '';
   ytdlpy = super.writeShellScriptBin "ytdlpy" ''
     exec "${self.youtube-dl}/bin/youtube-dl" --output "${ytdl-dir}/${playlist-video-fmt}" --yes-playlist "$@"
