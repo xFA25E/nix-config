c40cfe7af6294fafce91670d7a6c718814db026b
xFA25E <55886566+xFA25E@users.noreply.github.com>
2021-09-14 15:39:14 +0200

    nixpkgs: Add firefox to browser.nix and remove while loop
    Before, there was a while loop to force the decision of a browser on user.
    Maybe, it's too much.

--- overlays/browser.nix
+++ overlays/browser.nix
@@ -1,13 +1,15 @@
 self: super: {
   browser = super.writeShellScriptBin "browser" ''
     browser=""
-    while test -z "''${browser}"; do
-        case "$(printf 'brave\nytdl\nmpv\n' | '${self.dmenu}/bin/dmenu')" in
+    # when
+    if test -z "''${browser}"; then
+        case "$(printf 'firefox\nbrave\nytdl\nmpv\n' | '${self.dmenu}/bin/dmenu')" in
+            firefox) browser="${self.firefox}/bin/firefox" ;;
             brave) browser="${self.braveIncognito}/bin/brave-incognito" ;;
             ytdl) browser="${self.scripts}/bin/ytdli" ;;
             mpv) browser="${self.scripts}/bin/mpvi" ;;
         esac
-    done
+    fi
     "''${browser}" "$@"
   '';
 }
