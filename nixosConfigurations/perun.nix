18c8010c4fc7d0c2032b275468678562547a2ae5
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2025-05-02 10:55:56 +0200

    [nix] Remove mahogany from perun

--- nixosConfigurations/perun.nix
+++ nixosConfigurations/perun.nix
@@ -43,7 +43,11 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEbZ9Kz4oCbrybWc7jM6Oc7+UKFDsXtb/8IzmtpRb5flqXKy0ghUDLAQl/tur7du0HuX8la5Qsko/IbXN2ZK+2lqiWUnszAPA8P6DdLLO+U9W6yR5LqpIZLpDOwhQVf/IkrNEQXAGEP46YpYLLsn6SATQnXSy87Ri/au6+4joOMoQN9rjKPDD638BDDzFMf3fEbDotC1H5sBPHlrk09hsD4/pyrxmn7UJouT6cGWkuqXAx/NclGnay9hAsue00QqUYK62IC2wE9vNvmzWlAX2eVzo4envypNTe4tYDYS6BGklZP04AcgOwlVeOO+eUkPFMLgVSfJyNqpxXIugS05w9 vlr.ltkvsk@protonmail.com"
     ];
-    packages = [pkgs.mahogany pkgs.seatd pkgs.foot];
+    packages = [
+      # pkgs.mahogany
+      pkgs.seatd
+      pkgs.foot
+    ];
   };
 
   security.polkit.enable = true;
