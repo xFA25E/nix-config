aa47df6ec21568b86d96dbb2780cb5013a7804c4
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2025-05-10 19:22:46 +0200

    [nix] Add phone to make_backup script

--- overlays/make_backup.nix
+++ overlays/make_backup.nix
@@ -12,7 +12,7 @@ writeShellScriptBin "make_backup" ''
           --backup-dir="''${1%/}/backup" --suffix="$(date '+.~%Y%m%d~')" \
           --exclude='lost+found' --exclude="$1" --filter=':- .gitignore' \
           ~/Documents ~/Downloads ~/Music ~/Pictures ~/Videos ~/archives ~/org \
-          ~/roam \
+          ~/roam ~/phone \
           ~/.authinfo.gpg ~/.bash_history ~/.gnupg ~/.ssh \
           ~/.local/share/emacs ~/.local/share/mail ~/.local/share/password-store \
           ~/.config/transmission-daemon \
