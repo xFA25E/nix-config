fde33d4a42ddf88fb54dc088909a6889755a41f0
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2026-04-10 09:32:36 +0200

    veles: Add network conf for Anthem

--- nixosConfigurations/veles.nix
+++ nixosConfigurations/veles.nix
@@ -65,6 +65,21 @@
     useDHCP = lib.mkDefault true;
   };
 
+  networking.networkmanager.ensureProfiles.profiles = {
+    "Wired Connection 1" = {
+      connection.type = "ethernet";
+      connection.id = "Wired Connection 1";
+      connection.interface-name = "enp0s31f6"; # Make sure this matches your interface
+      connection.autoconnect = true;
+
+      ipv4.method = "manual";
+      ipv4.addresses = "192.168.10.200/24";
+      ipv4.gateway = "255.255.255.0";
+      ipv4.dns = "8.8.8.8";
+      ipv4.route-metric = 1000;
+    };
+  };
+
   swapDevices = [{device = "/swap/swapfile";}];
   system.stateVersion = "24.11";
 }
